84a43734ed583f4101f4ef19881aa15976801020
Romain Vimont <rom@rom1v.com>
2021-01-31 18:24:35 +0100

    thread

---
+++ app/src/util/thread.h
@@ -0,0 +1,66 @@
+#ifndef SC_THREAD_H
+#define SC_THREAD_H
+
+#include "common.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Forward declarations */
+typedef struct SDL_Thread SDL_Thread;
+typedef struct SDL_mutex SDL_mutex;
+typedef struct SDL_cond SDL_cond;
+
+typedef int sc_thread_fn(void *);
+
+struct sc_thread {
+    SDL_Thread *thread;
+};
+
+struct sc_mutex {
+    SDL_mutex *mutex;
+};
+
+struct sc_cond {
+    SDL_cond *cond;
+};
+
+bool
+sc_thread_create(struct sc_thread *thread, sc_thread_fn fn, const char *name,
+                 void *userdata);
+
+void
+sc_thread_join(struct sc_thread *thread, int *status);
+
+bool
+sc_mutex_init(struct sc_mutex *mutex);
+
+void
+sc_mutex_destroy(struct sc_mutex *mutex);
+
+void
+sc_mutex_lock(struct sc_mutex *mutex);
+
+void
+sc_mutex_unlock(struct sc_mutex *mutex);
+
+bool
+sc_cond_init(struct sc_cond *cond);
+
+void
+sc_cond_destroy(struct sc_cond *cond);
+
+void
+sc_cond_wait(struct sc_cond *cond, struct sc_mutex *mutex);
+
+// return true on signaled, false on timeout
+bool
+sc_cond_timedwait(struct sc_cond *cond, struct sc_mutex *mutex, uint32_t ms);
+
+void
+sc_cond_signal(struct sc_cond *cond);
+
+void
+sc_cond_broadcast(struct sc_cond *cond);
+
+#endif
